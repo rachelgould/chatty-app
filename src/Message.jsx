2487ade8fa983bfc711d0e204cd86398de15eb5e
Rachel Gould <rachegould@gmail.com>
2019-03-12 01:46:31 +0000

    use dummy message content in json doc

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,15 +1,52 @@
 import React, {Component} from 'react';
+import messages from './messages-sample.json';
 
-class Message extends Component {
+
+class MessageItem extends Component {
   render() {
     return (
       <div className="message">
-        <span className="message-username">Anonymous1</span>
-        <span className="message-content">I won't be impressed with technology until I can download food.</span>
+        <span className="message-username">{this.props.message.username}</span>
+        <span className="message-content">{this.props.message.content}</span>
       </div>
-      // <div class="message system">
-      //   Anonymous1 changed their name to nomnom.
-      // </div>
+    );
+  }
+}
+
+class NotificationItem extends Component {
+  render() {
+    return (
+      <div className="message system">
+      {this.props.message.content}
+      </div>
+    );
+  }
+}
+
+const chatMessages = messages.filter(message => {
+  return message.type === 'incomingMessage'
+});
+const notifications = messages.filter(message => {
+  return message.type === 'incomingNotification'
+});
+
+class Message extends Component {
+  constructor() {
+    super();
+    this.state = { chatMessages: chatMessages, notifications: notifications };
+  }
+  render() {
+    const messageItems = this.state.chatMessages.map(msg => (
+      <MessageItem message={msg} />
+    ));
+    const alerts = this.state.notifications.map(msg => (
+      <NotificationItem message={msg} />
+    ))
+    return (
+      <div>
+        {messageItems}
+        {alerts}
+      </div>
     );
   }
 }
