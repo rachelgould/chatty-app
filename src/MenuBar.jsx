890e30694c6cd7ffeeda7c877bec6e25b777884f
Rachel Gould <rachegould@gmail.com>
2019-03-14 19:40:43 +0000

    User counter formatting

--- src/MenuBar.jsx
+++ src/MenuBar.jsx
@@ -5,7 +5,7 @@ class Menu extends Component {
     return (
       <nav className="navbar">
         <a href="/" className="navbar-brand">Chatty App</a>
-        <p className="users-count">{this.props.users}</p>
+        <p className="users-count">{this.props.users} {this.props.users > 1 ? 'users' : 'user'} online</p>
       </nav>
     );
   }
